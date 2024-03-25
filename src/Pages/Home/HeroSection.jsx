5262aa8f54c89a7308f878ba531cb04f3a0d820b
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-25 10:52:21 -0400

    homepage layout and styling

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+    return (
+<section id="HeroSection" className="hero-section">
+<div className="hero-section-content-box">
+<div className="hero-section-content">
+    <p className="section-title"> Hey, Im Reagan</p>
+    <h1 className="hero-section-title">
+        <span className="hero-section-title-color"> Full Stack</span> {" "}
+        <br />
+        Developer
+    </h1>
+    <p className="hero-section-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sapiente deserunt quos debitis, eveniet nemo molestiae ex minima maxime nesciunt quidem laborum officia distinctio soluta aliquid ducimus sunt qui odit.
+        <br />  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+</div>
+<button className="btn btn-primary"> Get in touch</button>
+</div>
+<div className="hero-section-image">
+ <img src=".//img/pf img.PNG" alt=" Hero Section" />    
+</div>
+</section>
+    );
+       
+}
