5a25f085b146d6822f46780af67eb06bb0930e73
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-25 15:02:02 -0400

    portfolio and css

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,6 +1,7 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills"
 import AboutMe from "../AboutMe"
+import MyPortfolio from "../MyPortfolio";
 
 export default function Home () {
     return (
