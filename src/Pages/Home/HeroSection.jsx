e9cf1e333e0ca3a4b0915b0f120520948a84c25b
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-04-02 00:56:37 -0400

    projects update and resume link

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -4,19 +4,15 @@ export default function HeroSection() {
       <div className="hero--section--content--box">
         <div className="hero--section--content">
           <p className="section--title">Hey, I'm Reagan Lowe</p>
-          {/* <div class="picture--of--me">
-        <img src="path/to/your/image.jpg" alt="Reagan's Image" />
-      </div> */}
           <h1 className="hero--section--title">
             <span className="hero--section-title--color">Full Stack</span>{" "}
             <br />
             Developer
           </h1>
           <p className="hero--section-description">
-       
-Web Developer with over 22 applications developed, 23 If you count this one! Skilled in front-end and back-end technologies to deliver responsive and high-performing digital solutions. Experienced in utilizing APIs for data integration and management. Collaborative team player with a strong focus on meeting project requirements and delivering exceptional user experiences. <br></br>
+          Web Developer with a portfolio of 22+ web applications, 23 if you count this one! Skilled in a range of front-end technologies including HTML, CSS, and JavaScript frameworks such as React.js, Vue.js, Angular, and Bootstrap. Experienced in back-end development using Node.js, Express.js, and MongoDB/MySQL for database management. Ability to Integrate APIs for enhanced perfomace. A collaborative team player dedicated to delivering high-quality applications <br></br>
 
-In my free time, I enjoy playing soccer and working in freelance web development projects. I find joy in staying active in the field and continuously improving my skills in creating new projects.
+Outside of work and school I enjoy playing soccer as well as training younger players!
           </p>
           {/* Button positioned at the bottom-right corner */}
           <button
@@ -27,6 +23,10 @@ In my free time, I enjoy playing soccer and working in freelance web development
 >
   Get In Touch
 </button>
+<button className="btn btn-primary btn-resume">
+  <a href="https://docs.google.com/document/d/1P8SPYDx4JpSWeVSNrZrFGdGvF2WPbHjAwIc7kPw10Ks/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View my Resume</a>
+</button>
+
 
 </div>
       </div>
